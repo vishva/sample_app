85145beb86034b794a83b13f2aebfe7d30ac3847
Vishva <vishvazaccess@gmail.com>
2013-05-26 04:03:41 +0530

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,7 +30,7 @@ describe "User pages" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
-
+    
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Vishva Velichala"
@@ -41,6 +41,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+       describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+       
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
